d6b16f606409742317f969c0eed58a093c326d2a
DaniloTeo <danilo.teo@usp.br>
2020-04-14 11:43:29 -0300

    Implementação da leitura e escrita da imagem BMP está feita.

--- comp.c
+++ comp.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+// Start of Bitmap Data Structures *******************************************************
 typedef struct { 						/**** BMP file header structure ****/    
 	unsigned short bfType;			/* Magic number for file */ 
 	unsigned int   bfSize;			/* Size of file */ 
@@ -23,17 +25,114 @@ typedef struct{  /**** BMP file info structure ****/
 	unsigned int   biClrUsed;		/* Number of colors used */
 	unsigned int   biClrImportant;	/* Number of important colors */
 } BMPINFOHEADER;
+// End of Bitmap Data Structures ******************************************************
 
 
-void leituraHeader(FILE *F, BITMAPFILEHEADER *H){
+// Start of Bitmap Aux functions ******************************************************
+void leituraFileHeader(FILE *F, BMPFILEHEADER *H){
 	/* F é o arquivo Bitmap que deve ter sido “lido” do disco */
-	fread(&H->Type,sizeof (unsigned short int),1,F);
-	fread(&H->Size,sizeof (unsigned int),1,F);
-	fread(&H->Reserved1,sizeof (unsigned short int),1,F);
-	fread(&H->Reserved2,sizeof (unsigned short int),1,F); 
-	fread(&H->OffBits,sizeof (unsigned int),1,F);
+	fread(&H->bfType,sizeof (unsigned short int),1,F);
+	fread(&H->bfSize,sizeof (unsigned int),1,F);
+	fread(&H->bfReserved1,sizeof (unsigned short int),1,F);
+	fread(&H->bfReserved2,sizeof (unsigned short int),1,F); 
+	fread(&H->bfOffBits,sizeof (unsigned int),1,F);
 }
 
+void writeBMPFileHeader(FILE *F, BMPFILEHEADER *H){
+	fwrite(&H->bfType,sizeof (unsigned short int),1,F);
+	fwrite(&H->bfSize,sizeof (unsigned int),1,F);
+	fwrite(&H->bfReserved1,sizeof (unsigned short int),1,F);
+	fwrite(&H->bfReserved2,sizeof (unsigned short int),1,F); 
+	fwrite(&H->bfOffBits,sizeof (unsigned int),1,F);
+}
+
+void dumpFileHeader(BMPFILEHEADER *h){
+	printf("Magic Number of File: %hu\n", h->bfType);
+	printf("Size of File: %hu\n", h->bfSize);
+	printf("Reserved Var 1: %hu\n", h->bfReserved1);
+	printf("Reserved Var 2: %hu\n", h->bfReserved2);
+	printf("Offset to Bitmap Data: %hu\n", h->bfOffBits);
+	printf("-----+-----+-----+-----+-----+-----+\n");
+}
+
+void leituraInfoHeader(FILE *f, BMPINFOHEADER *h){
+	fread(&h->biSize, sizeof(unsigned int), 1, f);
+	fread(&h->biWidth, sizeof(int), 1, f);
+	fread(&h->biHeight, sizeof(int), 1, f);
+	fread(&h->biPlanes, sizeof(unsigned short), 1, f);
+	fread(&h->biBitCount, sizeof(unsigned short), 1, f);
+	fread(&h->biCompression, sizeof(unsigned int), 1, f);
+	fread(&h->biSizeImage, sizeof(unsigned int), 1, f);
+	fread(&h->biXPelsPerMeter, sizeof(int), 1, f);
+	fread(&h->biYPelsPerMeter, sizeof(int), 1, f);
+	fread(&h->biClrUsed, sizeof(unsigned int), 1, f);
+	fread(&h->biClrImportant, sizeof(unsigned int), 1, f);
+}
+
+void writeBMPInfoHeader(FILE *f, BMPINFOHEADER *h){
+	fwrite(&h->biSize, sizeof(unsigned int), 1, f);
+	fwrite(&h->biWidth, sizeof(int), 1, f);
+	fwrite(&h->biHeight, sizeof(int), 1, f);
+	fwrite(&h->biPlanes, sizeof(unsigned short), 1, f);
+	fwrite(&h->biBitCount, sizeof(unsigned short), 1, f);
+	fwrite(&h->biCompression, sizeof(unsigned int), 1, f);
+	fwrite(&h->biSizeImage, sizeof(unsigned int), 1, f);
+	fwrite(&h->biXPelsPerMeter, sizeof(int), 1, f);
+	fwrite(&h->biYPelsPerMeter, sizeof(int), 1, f);
+	fwrite(&h->biClrUsed, sizeof(unsigned int), 1, f);
+	fwrite(&h->biClrImportant, sizeof(unsigned int), 1, f);
+}
+
+void dumpInfoHeader(BMPINFOHEADER *h){
+	printf("Size of Info Header: %hu\n", h->biSize);
+	printf("Width of Image: %d\n", h->biWidth);
+	printf("Height of Image: %d\n", h->biHeight);
+	printf("No of Color Planes: %hu\n", h->biPlanes);
+	printf("No of Bits per Pixel: %hu\n", h->biBitCount);
+	printf("Type of Compression to Use: %u\n", h->biCompression);
+	printf("Size of Image Data: %u\n", h->biSizeImage);
+	printf("X Pixels per Meter: %d\n", h->biXPelsPerMeter);
+	printf("Y Pixels per Meter: %d\n", h->biYPelsPerMeter);
+	printf("No of Colors Used: %u\n", h->biClrUsed);
+	printf("No of Important Colors: %u\n", h->biClrImportant);
+	printf("-----+-----+-----+-----+-----+-----+\n");
+}
+// End of Bitmap Aux functions ******************************************************
+
+
+
+void readBMPImage(FILE *f, unsigned char ***B, unsigned char ***G, unsigned char ***R, int lin, int col){
+	int i = 0, j = 0;
+
+	for(i = 0; i < lin; i++){
+		for(j = 0; j < col; j++){
+			(*B)[i][j] = fgetc(f);
+			(*G)[i][j] = fgetc(f);
+			(*R)[i][j] = fgetc(f);
+		}
+	}
+}
+
+void writeBMPFile(unsigned char **B, unsigned char **G, unsigned char **R, BMPFILEHEADER *fileHeader, BMPINFOHEADER *infoHeader){
+	int i = 0, j = 0;
+	int lin = infoHeader->biHeight, col = infoHeader->biWidth;
+	FILE *out = fopen("out.bmp", "wb");
+	
+	// Escreve os cabeçalhos
+	writeBMPFileHeader(out, fileHeader);
+	writeBMPInfoHeader(out, infoHeader);
+	
+	// Escreve os dados da imagem
+	for(i = 0; i < lin; i++){
+		for(j = 0; j < col; j++){
+			fwrite(&R[i][j], sizeof(unsigned char), 1, out);
+			fwrite(&G[i][j], sizeof(unsigned char), 1, out);
+			fwrite(&B[i][j], sizeof(unsigned char), 1, out);
+		}
+	}
+
+	fclose(out);
+}	
 
 unsigned char **alocaMatrizUnChar(int lin, int col){
 	int i = 0;
@@ -75,6 +174,15 @@ void liberaMatrizDouble(double** matriz, int lin){
 	free(matriz);
 }	
 
+void printUnCharMatriz(unsigned char **m, int lin, int col){
+	int i,j;
+	for (i = 0; i < lin; i++){
+		for(j = 0; j < col; j++){
+			printf("%u ", m[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 
 
@@ -82,6 +190,35 @@ void liberaMatrizDouble(double** matriz, int lin){
 
 
 int main(void){
+	// Alocando memoria pra arquivo
+	FILE *f = fopen("./Imagens/3.bmp", "rb");
+	
+	// Leitura e Exibição do File Header
+	BMPFILEHEADER fileHeader;
+	leituraFileHeader(f, &fileHeader);
+	dumpFileHeader(&fileHeader);
+
+	// Leitura e Exibição do infoHeader
+	BMPINFOHEADER infoHeader;
+	leituraInfoHeader(f, &infoHeader);
+	dumpInfoHeader(&infoHeader);
+
+	// Leitura dos componentes B,G,R da imagem
+	unsigned char **B = alocaMatrizUnChar(infoHeader.biHeight, infoHeader.biWidth);
+	unsigned char **G = alocaMatrizUnChar(infoHeader.biHeight, infoHeader.biWidth);
+	unsigned char **R = alocaMatrizUnChar(infoHeader.biHeight, infoHeader.biWidth);
+	readBMPImage(f, &B, &G, &R,infoHeader.biHeight, infoHeader.biWidth);
+
+
+	writeBMPFile(B, G, R, &fileHeader, &infoHeader);
+	
+
+
+	// Liberacao de Memoria
+	liberaMatrizUnChar(R,infoHeader.biHeight);
+	liberaMatrizUnChar(G,infoHeader.biHeight);
+	liberaMatrizUnChar(B,infoHeader.biHeight);
+	fclose(f);
 	
 	return 0;
 }

f622e529be4e65160d7b8715954add706ea90f7f
DaniloTeo <danilo.teo@usp.br>
2020-05-18 20:47:30 -0300

    Adicionadas funcoes de interface com o output das funcoes anteriores do programa principal; Codigo agora retorna uma estrutura unica com as informacoes devidamente associadas

--- rle.c
+++ rle.c
@@ -1,66 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-double * encodeRLE(double *vet, int n, int *conts, int *conts_len, int *tam){
-	int i;
-	
-	//tamanho do vetor output
-	printf("Alocando novo vetor com o tamanho original...\n");
-	double * output = (double *)malloc(sizeof(double) * n);
-	
-	for(i = 0; i < n; i++){
-		int count = 1;
-
-		while(i < n - 1 && vet[i] == vet[i+1]){
-			count++;
-			i++;
-
-		}
-
-		printf("Inserindo valor no contador...\n");
-		conts[*conts_len] = count;
-		(*conts_len)+=1;
-		output[*tam] = vet[i];
-		(*tam)+=1;
-	}
-
-	printf("Fim do algoritmo principal...\n");
-	printf("\n\n");
-
-	output = (double *) realloc(output, sizeof(double) * (*tam));
+#include "rle.h"
 
 
-	return output;
+
+ENCODED_IMAGE *initializeEncoded(void){
+	ENCODED_IMAGE *img = (ENCODED_IMAGE *) malloc(sizeof(ENCODED_IMAGE));
+	img->info = NULL;
+	img->qtds = NULL;
+	img->len = 0;
+
+	return img;
 }
 
-
-double *decodeRLE(int *conts, int len, double *comp, int *decomp_len){
-	int i,j = 0;
-
-	//descobir o tamanho do vetor original
-	for(i = 0; i < len; i++){
-		(*decomp_len) += conts[i];
-	}
-
-	double *output = (double *) malloc(sizeof(double) * (*decomp_len));
-
-	for(i = 0; i < len; i++){
-		while(conts[i] > 0){
-			output[j] = comp[i];
-			conts[i] = conts[i]-1;
-			j++;
-		}
-	}
-
-	return output;
-
-
+void freeEncoded(ENCODED_IMAGE * img){
+	free(img->info);
+	free(img->qtds);
+	free(img);
 }
-
 
 void printVetorInt(int *vet, int n){
 	int i = 0;
-	printf("TAMANHO DO VETOR: %d\n", n);
+	printf("TAMANHO DO VETOR INTEIRO: %d\n", n);
 	printf("VETOR:\n");
 	for(i = 0; i < n; i++){
 		printf("%d ", vet[i]);
@@ -77,34 +38,138 @@ void printVetorDouble(double *vet, int n){
 	printf("\n");
 }
 
+void encodeRLE(double *vet, int n, ENCODED_IMAGE *out){
+	int i, count;
+	
+	// printf("Alocando novo vetor com o tamanho original...\n");
+	out->info = (double *)malloc(sizeof(double) * n);
+	out->qtds = (int *) calloc(n, sizeof(int));
+	
+	for(i = 0; i < n; i++){
+		count = 1;
+
+		while(i < n - 1 && vet[i] == vet[i+1]){
+			count++;
+			i++;
+
+		}
+
+		// printf("Inserindo valor no contador...\n");
+		out->qtds[out->len] = count;
+		out->info[out->len] = vet[i];
+		(out->len)+=1;
+	}
+
+	// printf("Fim do algoritmo principal...\n");
+	printf("\n\n");
+
+	out->info = (double *) realloc(out->info, sizeof(double) * (out->len));
+	
+	out->qtds = (int *) realloc(out->qtds, sizeof(double) * (out->len));
+}
+
+
+double *decodeRLE(ENCODED_IMAGE *input, int *decomp_len){
+	int i,j = 0;
+
+	//descobir o tamanho do vetor original
+	for(i = 0; i < input->len; i++){
+		(*decomp_len) += input->qtds[i];
+	}
+
+	double *output = (double *) malloc(sizeof(double) * (*decomp_len));
+
+	for(i = 0; i < input->len; i++){
+		while(input->qtds[i] > 0){
+			output[j] = input->info[i];
+			input->qtds[i] = input->qtds[i]-1;
+			j++;
+		}
+	}
+
+	return output;
+
+
+}
+
+ENCODED_IMAGE **encodeImage(double **img, int length){
+	int i;
+
+	ENCODED_IMAGE **output = (ENCODED_IMAGE **) malloc(sizeof(ENCODED_IMAGE *) * length);
+
+	for(i = 0; i < length; i++){
+		output[i] = initializeEncoded();
+		encodeRLE(img[i], (8*8), output[i]);
+	}
+
+	return output;
+}
+
+double **decodeImage(ENCODED_IMAGE **encoded, int length){
+	int i, decomp_len;
+	double **out = (double **) malloc(sizeof(double *) * length);
+	
+	for(i = 0; i < length; i++){
+		decomp_len = 0;
+		// printVetorInt(encoded[i]->qtds, encoded[i]->len);
+		out[i] = decodeRLE(encoded[i], &decomp_len);
+	}
+
+	return out;
+}
+
+
+
 /*
 int main(void){
 	// char string[] = "wwwwaaadexxxxxxywww";
-	double vet[13] = {32.0, 32.0, 12.2, 12.2, 12.2, 30.6, 42.42, 5.0,5.0,5.0, 41.2, 41.2, 41.2};
-	int *conts = (int *) calloc(13, sizeof(int));
-	int conts_len = 0, comp_len = 0;
-
-	printf("VETOR ORIGINAL:\n");
-	printVetorDouble(vet, 13);
-
-	printf("Comprimindo\n\n");
-	double * comp = encodeRLE(vet, 13, conts, &conts_len, &comp_len);
+	int i, j;
+	int width = 64, height = 8;
+	srand(time(NULL));
 
 
-	conts = (int *)realloc(conts, sizeof(int) * conts_len);
 
-	printVetorDouble(comp, comp_len);
-	printVetorInt(conts, conts_len);
+	double **test = (double **) malloc(sizeof(double *) * height);
+
+	for(i = 0; i < height; i++){
+		test[i] = (double *) malloc(sizeof(double) * width);
+		for(j = 0; j < width; j++){
+			test[i][j] = (double) (rand() % 3);
+		}
+	}
+
+	ENCODED_IMAGE **out = encodeImage(test, height);
+	
+	
+	for(i = 0; i < height; i++){
+		printf("ITERAÇÃO: %d\n", i);
+		printVetorDouble(out[i]->info, out[i]->len);
+		printf("\n\n");
+		printVetorInt(out[i]->qtds, out[i]->len);
+		printf("\n\n");
+		
+	}
+
+	double ** decoded = decodeImage(out, height);
+	for(i = 0; i < height; i++){
+		printf("ITERAÇÃO: %d\n", i);
+		printVetorDouble(decoded[i], width);
+		printf("\n\n");
+		
+	}
 
 
-	printf("Extraindo...\n");
-	int decomp_len = 0;
-	double *output = decodeRLE(conts, conts_len, comp, &decomp_len);
-	printVetorDouble(output, decomp_len);
+	for(i = 0; i < height; i++){
+		free(decoded[i]);
+		freeEncoded(out[i]);
+		free(test[i]);
+	}
 
-
-	free(output);
-	free(comp);
-	free(conts);
+	free(decoded);
+	free(out);
+	free(test);
+	
 	return 0;
-}*/
+
+}
+*/
--- rle.h
+++ rle.h
@@ -1,13 +1,31 @@
 #ifndef RLE_H_INCLUDED
 #define RLE_H_INCLUDED
 
-double * encodeRLE(double *vet, int n, int *conts, int *conts_len, int *tam);
 
-double * decodeRLE(int *conts, int len, double *comp, int *decomp_len);
+typedef struct{
+	double *info;
+	int *qtds;
+	int len;
+
+} ENCODED_IMAGE;
+
+
+ENCODED_IMAGE *initializeEncoded(void);
+
+void freeEncoded(ENCODED_IMAGE * img);
 
 void printVetorInt(int *vet, int n);
 
 void printVetorDouble(double *vet, int n);
+
+void encodeRLE(double *vet, int n, ENCODED_IMAGE *out);
+
+double *decodeRLE(ENCODED_IMAGE *input, int *decomp_len);
+
+ENCODED_IMAGE **encodeImage(double **img, int length);
+
+double **decodeImage(ENCODED_IMAGE **encoded, int length);
+
 
 
 #endif
